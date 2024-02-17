a374b2694bcfd6d16033876fa30dcb483bb848c2
CarolPe03 <anacarolpc11@gmail.com>
2024-02-17 00:42:01 -0600

    Capitulo 13

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,8 +17,7 @@ export async function fetchRevenue() {
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
+  
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<Revenue>`SELECT * FROM revenue`;
@@ -168,6 +167,7 @@ export async function fetchInvoiceById(id: string) {
     }));
 
     return invoice[0];
+    console.log(invoice); 
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoice.');
